2993e3ed114993c1ba09c61d99352c883642502c
xwq1012 <627455689@qq.com>
2020-11-19 19:21:40 +0800

    删除无用代码

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Author xwq
- * @Date 2020/11/19 11:35
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- src/Test1.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Author xwq
- * @Date 2020/11/19 17:11
- */
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("12323");
-    }
-}
