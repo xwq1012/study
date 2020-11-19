037b7e043eeea4f96d9bd1f9f36478739c7578a1
xwq1012 <627455689@qq.com>
2020-11-19 17:43:09 +0800

    提交到本地仓库

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @Author xwq
+ * @Date 2020/11/19 11:35
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * @Author xwq
+ * @Date 2020/11/19 17:11
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
